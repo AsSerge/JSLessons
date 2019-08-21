6de9dbca4c0f8e57e5113c1937ca28057135198f
Serge <z00m.serge@gmail.com>
2019-08-21 20:43:00 +0300

    Second

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
-"use strict";
-var rt = "Привет";
-alert(rt)
+// s
+// console.log(person['name']);
+// let arr = ['plum.png', 'orange.jpg', 'apple.png', 'boo.txt'];
+// console.log(arr[1]);
+// alert('Hellow');
+// let answer = confirm('Who is &');
+// console.log(answer);
+
+// let answer = prompt('Who is &', 'Yes');
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 100;
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
